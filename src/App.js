86bd5a8a7722bc09e0e860c8089f0fe68267250b
sarojlimbu <limbusaroz19@gmail.com>
2022-05-14 21:18:19 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
 import './App.css';
 
-// import { ThemeProvider } from '@mui/styles';
-// import { theme } from './theme';
-import { Stack } from '@mui/material';
-import Display from './components/Display';
-import Racks from './components/Racks';
-import Navbar from './components/Navbar';
+import Layout from "./components/Layout";
+import { ThemeProvider } from '@mui/styles';
+import { theme } from './theme';
+
 
 
 function App() {
   return (
-    // <ThemeProvider theme={theme}>
-    //   <Layout/>
-    // </ThemeProvider>
-    <div>
-    <Navbar/>
-    <Stack direction="row">
-  
-      <Racks/>
-      
-      <Display />
+    <ThemeProvider theme={theme}>
+      <Layout/>
+    </ThemeProvider>
 
-      
-    </Stack>
-    </div>
 
-  );
+  )
 }
 
 export default App;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,175 +1,175 @@
-// import React from 'react';
-// import { Badge, Box, Button, Container, Drawer,List,ListItem, Typography } from '@mui/material';
-// import { TreeView} from '@mui/lab';
-// import { TreeItem } from '@mui/lab';
-// import { Toolbar } from '@mui/material';
-// import { AppBar } from '@mui/material';
-// import NotificationsNoneRoundedIcon from '@mui/icons-material/NotificationsNoneRounded';
-// import { makeStyles } from '@mui/styles';
-// import { useState } from 'react';
-// import { Menu,MenuItem } from '@mui/material';
+import React from 'react';
+import { Box, Button, Container, Drawer,List,ListItem, Typography } from '@mui/material';
+import { TreeView} from '@mui/lab';
+import { TreeItem } from '@mui/lab';
+import { Toolbar } from '@mui/material';
+import { AppBar } from '@mui/material';
+import NotificationsNoneRoundedIcon from '@mui/icons-material/NotificationsNoneRounded';
+import { makeStyles } from '@mui/styles';
+import { useState } from 'react';
+import { Menu,MenuItem } from '@mui/material';
 
-// const widthList="300px";
+const widthList="300px";
 
 
-// const useStyles= makeStyles({
+const useStyles= makeStyles({
 
-//     drawer:{
-//         width:widthList
-//     },
-//     appbar:{
-//         height:"5rem"
+    drawer:{
+        width:widthList
+    },
+    appbar:{
+        height:"5rem"
         
-//     },
-//     logo:{
-//         flexGrow:1
-//     }
-// })
+    },
+    logo:{
+        flexGrow:1
+    }
+})
 
 
 
-// const Layout = () => {
+const Layout = () => {
 
-//     const [open,setOpen]=useState(false)
+    const [open,setOpen]=useState(false)
 
-//     const classes=useStyles()
+    const classes=useStyles()
 
-//   return (
-//     <Box>
+  return (
+    <Box>
 
 
-//     {/* app bar */}
-//     <Container>
-//     <AppBar className={classes.appbar}
-//      sx={{
-//          backgroundColor:"#16003B",
-//          zIndex:1201,
-//          paddingTop:"0.5rem"
+    {/* app bar */}
+    <Container>
+    <AppBar className={classes.appbar}
+     sx={{
+         backgroundColor:"#16003B",
+         zIndex:1201,
+         paddingTop:"0.5rem"
         
-//      }}
-//     >
-//         <Toolbar>
-//             <Typography
-//                 className={classes.logo}
-//             >
-//                 LOGO
-//             </Typography>  
-//                 <Button startIcon={<NotificationsNoneRoundedIcon/>} variant='outline'>Notification</Button> 
+     }}
+    >
+        <Toolbar>
+            <Typography
+                className={classes.logo}
+            >
+                LOGO
+            </Typography>  
+                <Button startIcon={<NotificationsNoneRoundedIcon/>} variant='outline'>Notification</Button> 
             
-//             <Button  onClick={e=>setOpen(true)} variant="outline">Danson Demo</Button>
+            <Button  onClick={e=>setOpen(true)} variant="outline">Danson Demo</Button>
             
-//         </Toolbar>
+        </Toolbar>
 
-//         {/* logout menu */}
-//         <Menu
-//         id="demo-positioned-menu"
-//         aria-labelledby="demo-positioned-button"
+        {/* logout menu */}
+        <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
  
-//         open={open}
-//         onClose={e=>setOpen(false)}
-//         anchorOrigin={{
-//           vertical: 'top',
-//           horizontal: 'right',
-//         }}
-//         transformOrigin={{
-//           vertical: 'top',
-//           horizontal: 'right',
-//         }}
-//       >
-//         <MenuItem >Profile</MenuItem>
-//         <MenuItem >My account</MenuItem>
-//         <MenuItem >Logout</MenuItem>
-//       </Menu>
+        open={open}
+        onClose={e=>setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem >Profile</MenuItem>
+        <MenuItem >My account</MenuItem>
+        <MenuItem >Logout</MenuItem>
+      </Menu>
 
-//     </AppBar>
-//     </Container>
+    </AppBar>
+    </Container>
 
-//     {/* side drawer */}
+    {/* side drawer */}
         
-//         <Drawer
-//          button        
-//          variant='permanent'
-//          anchor="left"
-//         >
-//             <Toolbar/>
+        <Drawer
+         button        
+         variant='permanent'
+         anchor="left"
+        >
+            <Toolbar/>
      
-//             <List  className={classes.drawer}
-//                 sx={{
-//                     marginTop:"2rem"
-//                 }}
-//             >
-//                 <ListItem>
-//                     <TreeView>
-//                         <TreeItem  nodeId='1' label="Dashboard"/>
-//                     </TreeView>
-//                 </ListItem>
-//                 <ListItem>
-//                     <TreeView>
-//                         <TreeItem  nodeId='2' label="Customer Management">
-//                             <TreeItem  nodeId='22' label="Customer Profile"/>
-//                         </TreeItem>
-//                     </TreeView>
-//                 </ListItem>
-//                 <ListItem>
-//                     <TreeView>
-//                         <TreeItem  nodeId='3' label="Fund Management"/>
-//                     </TreeView>
-//                 </ListItem>
-//                 <ListItem>
-//                     <TreeView>
-//                         <TreeItem  nodeId='4' label="Transaction Management">
-//                             <TreeItem  nodeId='44' label="Bond"/>
-//                             <TreeItem  nodeId='444' label="Equity"/>
-//                         </TreeItem>
-//                     </TreeView>
-//                 </ListItem>
-//                 <ListItem>
-//                     <TreeView>
-//                         <TreeItem  nodeId='5' label="Company Management">
-//                             <TreeItem  nodeId='55' label="Company Profile" />
-//                             <TreeItem  nodeId='555' label="Security Profile" />
-//                             <TreeItem  nodeId='5555' label="Security Snap" />
+            <List  className={classes.drawer}
+                sx={{
+                    marginTop:"2rem"
+                }}
+            >
+                <ListItem>
+                    <TreeView>
+                        <TreeItem  nodeId='1' label="Dashboard"/>
+                    </TreeView>
+                </ListItem>
+                <ListItem>
+                    <TreeView>
+                        <TreeItem  nodeId='2' label="Customer Management">
+                            <TreeItem  nodeId='22' label="Customer Profile"/>
+                        </TreeItem>
+                    </TreeView>
+                </ListItem>
+                <ListItem>
+                    <TreeView>
+                        <TreeItem  nodeId='3' label="Fund Management"/>
+                    </TreeView>
+                </ListItem>
+                <ListItem>
+                    <TreeView>
+                        <TreeItem  nodeId='4' label="Transaction Management">
+                            <TreeItem  nodeId='44' label="Bond"/>
+                            <TreeItem  nodeId='444' label="Equity"/>
+                        </TreeItem>
+                    </TreeView>
+                </ListItem>
+                <ListItem>
+                    <TreeView>
+                        <TreeItem  nodeId='5' label="Company Management">
+                            <TreeItem  nodeId='55' label="Company Profile" />
+                            <TreeItem  nodeId='555' label="Security Profile" />
+                            <TreeItem  nodeId='5555' label="Security Snap" />
 
-//                         </TreeItem>
-//                     </TreeView>
-//                 </ListItem>
-//                 <ListItem>
-//                     <TreeView>
-//                         <TreeItem  nodeId='6' label="User Management">
-//                         <TreeItem  nodeId='66' label="Add Users" />
-//                         <TreeItem  nodeId='666' label="Users" />
-//                         </TreeItem>
-//                     </TreeView>
-//                 </ListItem>
-//                 <ListItem>
-//                     <TreeView>
-//                         <TreeItem  nodeId='7' label="Region Management">
-//                         <TreeItem  nodeId='77' label="Country" />
-//                         <TreeItem  nodeId='777' label="City" />
-//                         </TreeItem>
-//                     </TreeView>
-//                 </ListItem>
-//                 <ListItem>
-//                     <TreeView>
-//                         <TreeItem  nodeId='8' label="Settings">
-//                             <TreeItem  nodeId='88' label="Service Charge" />
-//                             <TreeItem  nodeId='888' label="Master Settings" />
-//                         </TreeItem>
-//                     </TreeView>
-//                 </ListItem>
-//                 <ListItem>
-//                     <TreeView>
-//                         <TreeItem  nodeId='9' label="Reports">
-//                         <TreeItem  nodeId='99' label="Transaction Report" />
-//                         <TreeItem  nodeId='999' label="Fund Report" />
-//                         </TreeItem>
-//                     </TreeView>
-//                 </ListItem>
-//             </List>
+                        </TreeItem>
+                    </TreeView>
+                </ListItem>
+                <ListItem>
+                    <TreeView>
+                        <TreeItem  nodeId='6' label="User Management">
+                        <TreeItem  nodeId='66' label="Add Users" />
+                        <TreeItem  nodeId='666' label="Users" />
+                        </TreeItem>
+                    </TreeView>
+                </ListItem>
+                <ListItem>
+                    <TreeView>
+                        <TreeItem  nodeId='7' label="Region Management">
+                        <TreeItem  nodeId='77' label="Country" />
+                        <TreeItem  nodeId='777' label="City" />
+                        </TreeItem>
+                    </TreeView>
+                </ListItem>
+                <ListItem>
+                    <TreeView>
+                        <TreeItem  nodeId='8' label="Settings">
+                            <TreeItem  nodeId='88' label="Service Charge" />
+                            <TreeItem  nodeId='888' label="Master Settings" />
+                        </TreeItem>
+                    </TreeView>
+                </ListItem>
+                <ListItem>
+                    <TreeView>
+                        <TreeItem  nodeId='9' label="Reports">
+                        <TreeItem  nodeId='99' label="Transaction Report" />
+                        <TreeItem  nodeId='999' label="Fund Report" />
+                        </TreeItem>
+                    </TreeView>
+                </ListItem>
+            </List>
 
-//         </Drawer>
-//     </Box>
-//   )
-// }
+        </Drawer>
+    </Box>
+  )
+}
 
-// export default Layout;
+export default Layout;
